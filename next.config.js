7b74b7026a4ac9b47104a0d1064fcd643031b41f
fredy <fredyalmenares@gmail.com>
2022-04-11 15:25:02 -0300

    Added Traefik

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = (phase, { defaultConfig }) => {
      */
     const nextConfig = {
         /* config options here */
+        basePath: process.env.API_PREFIX,
     }
     return nextConfig
 }
